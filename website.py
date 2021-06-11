4c25259f719c0604f599b55052651f76f8a45f31
tuan.nmt <tuanxoantit@gmail.com>
2021-06-11 17:09:05 +0700

    Load Html

--- website.py
+++ website.py
@@ -4,19 +4,24 @@ import os
 
 mApp = Flask(__name__)
 
+mWeb = ['Shopee', 'Tiki', 'Mediamart', 'Dien May Xanh', 'Lazada']
+
+
 @mApp.route('/')
 def Homepage():
-    return render_template('Search.html')
+    return render_template('Search.html', mSelected = [0, 1, 2, 3, 4], mWebSearch = mWeb)
 
 @mApp.route('/Result',methods=['POST','GET'])
 def Result():
     # mTextSearch = request.form['mTextSearch']
     mTextSearch = request.form.get("mTextSearch", False)
+    selected = request.form.getlist('mWeb')
+    selected = list(map(int, selected))
     try :        
         mComparePrices = Prices(mTextSearch)
         mData = mComparePrices.GetData()
     
-        return render_template('Result.html', mResult = mData, TextSearch = mTextSearch)
+        return render_template('Result.html', mResult = mData, TextSearch = mTextSearch, mSelected = selected, mWebSearch = mWeb)
     except:
         return 'No search results'
 
