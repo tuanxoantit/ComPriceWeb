32ac3aa21d781abe3f1d78d58b9279a0f7b8d867
Nguyễn Mạnh Tuấn <tuanxoantit@gmail.com>
2021-06-11 08:20:57 +0700

    Update Filter Web

--- ComparePrices.py
+++ ComparePrices.py
@@ -1,35 +1,56 @@
-from bs4 import BeautifulSoup
 import requests
+import json
 
 class Prices():
     mHeaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     def __init__(self, mProduct):
         self.mProduct = mProduct
-        self.mUrl ='https://www.google.com/search?q={0}&tbm=shop'.format(self.mProduct )  
-        # self.mUrl = 'https://www.google.com/search?q=card+man+hinh&tbm=shop' 
-    
-    def GetData(self):  
-        mRes = requests.get(self.mUrl, headers = self.mHeaders)
-        mSoup = BeautifulSoup(mRes.text,'html.parser')
+        self.mUrl ='https://shopee.vn/api/v4/search/search_items?by=relevancy&keyword={0}&limit=10&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2'.format(self.mProduct)
+        self.mUrlShopee = 'https://shopee.vn/api/v4/search/search_items?by=relevancy&keyword={0}&limit=10&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2'.format(self.mProduct)
+        self.mUrlLazada= 'https://www.lazada.vn/catalog/?q=={0}'.format(self.mProduct )
+        self.mUrlTiki = 'https://tiki.vn/api/v2/products?limit=10&include=advertisement&aggregations=1&trackity_id=5e241048-5c36-3899-05d6-d9c8af97c3ac&q={0}'.format(self.mProduct )
 
-        mSpanPrices = mSoup.find_all('span', {'class' : 'a8Pemb'})
-        mStrPrices = [mPrice.contents[0] for mPrice in mSpanPrices ]
-        mPrices = [int(mPrice.contents[0].strip('\xa0₫').replace('.','')) for mPrice in mSpanPrices ]
+    def Shopee(self):  
+        mData = json.loads(requests.get(self.mUrlShopee, headers = self.mHeaders).text)
+        # mProducts = [[mProduct['item_basic']['name'], mProduct['item_basic']['price'], f'https://shopee.vn/a-i.{mProduct["shopid"]}.{mProduct["itemid"]}'] for mProduct in mData['items']]
+        mProducts = []
+        for mProduct in mData['items']:
+            mName = mProduct['item_basic']['name']
+            mPrice = int(str(mProduct['item_basic']['price'])[:-5])
+            mStrPrice = self.ThousandsSeparator(mPrice)
+            mLink = f'https://shopee.vn/a-i.{mProduct["shopid"]}.{mProduct["itemid"]}'
+            mDictProduct = [mName, mPrice, mStrPrice, mLink, "Shopee"]
+            mProducts.append(mDictProduct)
+        return mProducts    
 
-        mDivStores = mSoup.find_all('div', {'class' : 'mqQL1e'})
-        mStores = [mStore.find_next('span').string for mStore  in mDivStores] 
+    def Tiki(self):
+        mRawData = requests.get(self.mUrlTiki, headers = self.mHeaders).text
+        mData = json.loads(requests.get(self.mUrlTiki, headers = self.mHeaders).text)
+        # mProducts = [[mProduct['name'], mProduct['price'], f'https://tiki.vn/{mProduct["url_path"]}'] for mProduct in mData['data']]
+        mProducts = []
+        for mProduct in mData['data']:
+            mName = mProduct['name']
+            mPrice = int(mProduct['price'])
+            mStrPrice = self.ThousandsSeparator(mPrice)
+            mLink = f'https://tiki.vn/{mProduct["url_path"]}'
+            mDictProduct = [mName, mPrice, mStrPrice, mLink, 'Tiki']
+            mProducts.append(mDictProduct)
+        return mProducts
 
-        mALinks = mSoup.find_all('a', {'class' : 'translate-content'})
-        mLinks = [mLink['href'] for mLink in mALinks ]
+    def ThousandsSeparator(self, mInput):
+        mData = '{:20,.0f}'.format(mInput) + " VND"
+        return mData
 
-        # print(mPrices)
-        # print(mStores)
-        # print(mLinks)
-        return mPrices, mStrPrices, mStores, mLinks
-        
-    def Result(self):
-        mPrices, mStrPrices, mStores, mLinks = self.GetData()
-        mData = [[StrPrices, Store, 'https://www.google.com{0}'.format(Link)] for Price, StrPrices, Store, Link in sorted(zip(mPrices, mStrPrices, mStores, mLinks))]
+    def GetData(self):
+        mShopeeData = self.Shopee()
+        mTikiData = self.Tiki()
+        mData = mShopeeData + mTikiData
+        mData = sorted(mData, key=lambda l:l[1])
+        mData = [{'Name': mData[0] ,'Price': mData[2], 'Link' : mData[3], 'Store': mData[4]} for mData in mData]
         return mData
 
 
+# mPrice = Prices('book')
+# mPrice.Shopee()
+# mPrice.GetData()
+# mPrice.ThousandsSeparator(100000000000000)
--- website.py
+++ website.py
@@ -1,26 +1,25 @@
 from flask import Flask, render_template, request
-import os
-
 from ComparePrices import Prices
-
+import os
 
 mApp = Flask(__name__)
 
 @mApp.route('/')
-def Index():
+def Homepage():
     return render_template('Search.html')
 
-@mApp.route('/Result',methods=['POST'])
+@mApp.route('/Result',methods=['POST','GET'])
 def Result():
-    mTextSearch = request.form['text']
-    try :
+    # mTextSearch = request.form['mTextSearch']
+    mTextSearch = request.form.get("mTextSearch", False)
+    try :        
         mComparePrices = Prices(mTextSearch)
-        mResult = mComparePrices.Result()
-        mDataOut = {'mData': mResult}
-        
-        return render_template('Result.html', mResultHtml = mDataOut, mTextSearchHtml = mTextSearch)
+        mData = mComparePrices.GetData()
+    
+        return render_template('Result.html', mResult = mData, TextSearch = mTextSearch)
     except:
         return 'No search results'
+
 
 if __name__ == '__main__':
     mApp.run(debug=True)
